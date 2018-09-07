cb49c26ffdef5d17aafc1b0d07c409d4462e1e3a
yakov <jasha-kz@rambler.ru>
2018-09-07 14:45:44 +0300

    added license refs and simple readme

---
+++ README.md
@@ -0,0 +1,40 @@
+                          Bitmask 2.0
+
+  What is it?
+  -----------
+
+  Bitmask is a simple tool to view boolean bit-values of 32-bit integer 
+  number by masks with freely available source code.
+
+  The Latest Version
+  ------------------
+
+  Latest version is Bitmask 2.0.0.0.
+  
+
+  Documentation
+  -------------
+
+
+  Installation
+  ------------
+
+
+  Licensing
+  ---------
+
+  GPL version 3
+  https://www.gnu.org/licenses/
+
+  Cryptographic Software Notice
+  -----------------------------
+
+  Contacts
+  --------
+
+     o If you have a concrete bug report for Bitmask please 
+       write to <jasha-kz@rambler.ru>
+
+     o If you want to participate in actively developing Bitmask please
+       visit to https://github.com/jashaustiuzhanin/BITMASK2
+
--- SRC/settings.cpp
+++ SRC/settings.cpp
@@ -1,3 +1,31 @@
+/***************************************************************************
+**                                                                        **
+**  Filename: settings.cpp                                                **
+**  Last updated [dd/mm/yyyy]: 07/09/2018                                 **
+**                                                                        **
+**  This file is part of Bitmask project                                  **
+**  (https://github.com/jashaustiuzhanin/BITMASK2).                       **
+**  Bitmask is a simple tool to view boolean bit-values of 32-bit integer **
+**  number by masks.                                                      **
+**                                                                        **
+**  Copyright (C) 2018 Jasha Ustiuzhanin                                  **
+**                                                                        **
+**  Bitmask is free software: you can redistribute it and/or modify       **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  Bitmask is distributed in the hope that it will be useful,            **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with Bitmask package (in the file COPYING).                     **
+**  If not, see <https://www.gnu.org/licenses/>.                          **
+**                                                                        **
+****************************************************************************/
+
 #include <QtCore>
 #include <QColor>
 #include <QFileDialog>
